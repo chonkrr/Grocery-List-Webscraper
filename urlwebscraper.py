9a815df6e9d94871b5bb91943dcf6b39f08e3b9c
Davis Foley <Davis.Foley@du.edu>
2025-02-25 22:38:52 -0700

    created model grocery list for testing

--- urlwebscraper.py
+++ urlwebscraper.py
@@ -1,12 +1,16 @@
 from playwright.sync_api import sync_playwright
 
-def extract_urls():
+def extract_urls(searchterm):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, args=["--disable-http2"])  # Change to True if you don't want to see the browser
         page = browser.new_page()
 
         # Go to the provided search page URL
-        search_url = "https://www.kingsoopers.com/search?query=eggs&searchType=default_search"
+        urlstart = "https://www.kingsoopers.com/search?query="
+        urlend = "&searchType=default_search"
+        #searchterm = "milk"
+        search_url = urlstart + searchterm + urlend
+        #search_url = "https://www.kingsoopers.com/search?query=eggs&searchType=default_search"
         page.goto(search_url)
 
         # Locate all <a> tags that contain the product links based on the href pattern
@@ -17,6 +21,8 @@ def extract_urls():
             links = elements[1:4]
             for link in links:
                 url = link.get_attribute('href')
+                if not url.__contains__(searchterm):
+                    continue
                 full_url = page.url.split('/search?')[0] + url  # Append the relative URL to the base URL
                 print(full_url)
         else:
@@ -24,4 +30,4 @@ def extract_urls():
 
         browser.close()
 
-extract_urls()
+extract_urls("milk")
